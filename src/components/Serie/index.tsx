d3377bef0faa65206f84ca6a6e06953ed65cd162
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-16 16:23:32 -0300

    feat: criando o componente Serie

---
+++ src/components/Serie/index.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useState } from 'react'
+import { SerieModal } from '../SerieModal'
+
+import * as S from './styles'
+
+export type SerieProps = {
+  id: number
+  img: string
+  title: string
+  description: string
+  firstAirDate: string
+}
+
+export function Serie({
+  id,
+  img,
+  title,
+  description,
+  firstAirDate
+}: SerieProps) {
+
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const openModal = useCallback(() => {
+    setIsOpenModal(true)
+  }, [])
+
+  const closeModal = useCallback(() => {
+    setIsOpenModal(false)
+  }, [])
+
+  return (
+    <>
+      <S.Container onClick={openModal}>
+        <S.Image src={img} alt={title} />
+        <S.Content>
+          <S.Title>{title}</S.Title>
+          <S.ReleaseDate>{firstAirDate}</S.ReleaseDate>
+          <S.Description>{description}</S.Description>
+        </S.Content>
+      </S.Container >
+      <SerieModal
+        isOpenModal={isOpenModal}
+        closeModal={closeModal}
+        serieId={id}
+      />
+    </>
+  )
+}
