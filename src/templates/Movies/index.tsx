95313733a9d4341b63f75fbdafe4e7781e412977
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-17 10:12:11 -0300

    chore: adicionando um title em cada pagina

--- src/templates/Movies/index.tsx
+++ src/templates/Movies/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useEffect } from 'react';
 import { Movie, MovieProps } from "../../components/Movie";
 import { useMovies } from '../../hooks/useMovies';
@@ -22,17 +23,22 @@ export function Movies({ movies }: MoviesTemplateProps) {
   }, [])
 
   return (
-    <BaseTemplate>
-      <S.Title>Filmes Populares</S.Title>
-      {(totalResults || movies)?.map(movie => (
-        <Movie {...movie} key={movie.id} />
-      ))}
-      <S.ShowMore onClick={nextPage}>
-        Ver mais
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-        </svg>
-      </S.ShowMore>
-    </BaseTemplate>
+    <>
+      <Head>
+        <title>TMDB | Filmes Populares</title>
+      </Head>
+      <BaseTemplate>
+        <S.Title>Filmes Populares</S.Title>
+        {(totalResults || movies)?.map(movie => (
+          <Movie {...movie} key={movie.id} />
+        ))}
+        <S.ShowMore onClick={nextPage}>
+          Ver mais
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>
+        </S.ShowMore>
+      </BaseTemplate>
+    </>
   )
 }
--- src/templates/Series/index.tsx
+++ src/templates/Series/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Serie, SerieProps } from "../../components/Serie";
 import { useSeries } from "../../hooks/useSeries";
 import { BaseTemplate } from "../Base";
@@ -14,15 +15,20 @@ export function Series({ series }: SeriesTemplateProps) {
   const { isLoading } = useSeries()
 
   return (
-    <BaseTemplate>
-      <S.Title>Séries Populares</S.Title>
-      {
-        isLoading
-          ? (<SkeletronPage />)
-          : series?.map(serie => (
-            <Serie {...serie} key={serie.id} />
-          ))
-      }
-    </BaseTemplate>
+    <>
+      <Head>
+        <title>TMDB | Séries Populares</title>
+      </Head>
+      <BaseTemplate>
+        <S.Title>Séries Populares</S.Title>
+        {
+          isLoading
+            ? (<SkeletronPage />)
+            : series?.map(serie => (
+              <Serie {...serie} key={serie.id} />
+            ))
+        }
+      </BaseTemplate>
+    </>
   )
 }
